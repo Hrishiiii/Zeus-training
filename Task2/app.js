452811fe5ae955894e2e95dc0cb6c5e9c0234a71
hrishikesh <hshinde166@gmail.com>
2022-06-24 23:42:58 +0530

    Updated javascript file for task2.

---
+++ Task2/app.js
@@ -0,0 +1,37 @@
+const input = document.querySelector(".inp");
+const textArea = document.querySelector(".text-area");
+const radio = document.myForm.radio; //Selecting the elements by their name attributes.
+
+const form = document.getElementById("form");
+
+form.addEventListener("submit", (e) => {
+  flag = 0; //Set flag to 0 initially
+
+  //If the inputs value or the textarea's value is empty then we show an alert
+  if (input.value === "" || textArea.value === "") {
+    //Set the focus whichever field is empty between input and textArea.
+    if (input.value === "") {
+      input.focus();
+    } else {
+      textArea.focus();
+    }
+    alert("All fields are compulsory");
+    return false;
+  }
+
+  //Now we loop through the radio buttons that we have to check whether atleast one of them is selected.
+  for (i = 0; i < radio.length; i++) {
+    if (radio[i].checked == true) {
+      flag = 1; //if selected we set flag to 1
+    }
+  }
+
+  //If flag is one then everythings fine else alert "All fields are required"
+  if (flag == 1) {
+    return true;
+  }
+
+  alert("All fields are compulsory");
+
+  e.preventDefault();
+});
