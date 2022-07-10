96c1ac73f54fb6d8c56f8b36a60be8f134fb6707
Hrishikesh Shinde <hrishikesh.shinde@zeuslearning.mumbai>
2022-07-11 00:43:01 +0530

    JS and css files for the items that get displayed when we hover over the alert icon.

---
+++ Task4/alertItems.js
@@ -0,0 +1,61 @@
+const alertItems = [
+    {
+        topic:"License for Introduction to Algebra has been assigned to your school",
+        img:false,
+        course:"",
+        time:"15-Sep-2018 at 07:21 pm"
+    },
+    {
+        topic:"Lesson 3 Practice Worksheet overdue for Amy Santiago",
+        img:true,
+        course:"Course: Advanced Mathematics",
+        time:"15-Sep-2018 at 05:21 pm"
+    },
+    {
+        topic:"23 new students created",
+        img:false,
+        course:"",
+        time:"14-Sep-2018 at 01:21 pm"
+    },
+    {
+        topic:"15 submissions ready for evaluation",
+        img:false,
+        course:"Course: Basics of algebra",
+        time:"13-Sep-2018 at 01:15 pm"
+    },
+    {
+        topic:"License for Basic Concepts in Geometry has been assigned to your...",
+        img:false,
+        course:"",
+        time:"15-Sep-2018 at 07:21 pm"
+    },
+    {
+        topic:"Lesson 3 Practice Worksheet overdue for Sam Diego",
+        img:false,
+        course:"Course: Advanced Mathematics",
+        time:"15-Sep-2018 at 07:21 pm"
+    }
+];
+
+function alertTemplate(data){
+    return(
+        `
+        <div class="main-box">
+            <div class="heading">
+                <span class="alert-topic">${data.topic}</span>
+                <img src="${data.img ? "icons/checkbox-checked.svg":""}" />
+            </div>
+            <span class="courses">${data.course}</span>
+            <p class="time">${data.time}</p>
+        </div>
+        `
+    )
+};
+
+document.querySelector(".alert-items").innerHTML = `
+${alertItems.map(alertTemplate).join("")}
+`;
+
+
+
+    
