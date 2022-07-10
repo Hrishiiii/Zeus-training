6fe9b94dc1b0fefb759da50054a76cb274ad772d
Hrishikesh Shinde <hrishikesh.shinde@zeuslearning.mumbai>
2022-07-11 00:42:07 +0530

    JS and css files for the items that get displayed when we hover over announcement icon.

---
+++ Task4/announcementItems.js
@@ -0,0 +1,73 @@
+const announceItems = [
+
+    {
+        teacherName : "Wilson Kumar",
+        information : "No classes will be held on 21st Nov",
+        course : false,
+        files : "2 files are attached",
+        timing : "15-sep-2018 at 07:21 pm"
+    },
+    {
+        teacherName : "Samson White",
+        information : "Guest lecture on Geometry on 20th September",
+        course : false,
+        files : "2 files are attached",
+        timing : "15-Sep-2018 at 07:21 pm"
+    },
+    {
+        teacherName : "Wilson Kumar",
+        information : "Additional course materials available on request",
+        course : true,
+        files : "",
+        timing : "15-sep-2018 at 07:21 pm"
+    },
+    {
+        teacherName : "Wilson Kumar",
+        information : "No classes will be held on 25th Dec",
+        course : false,
+        files : "",
+        timing: "15-sep-2018 at 07:21 pm"
+    },
+    {
+        teacherName : "Wilson Kumar",
+        information : "Additional course materials available on request",
+        course : true,
+        files : "",
+        timing : "15-sep-2018 at 07:21 pm"
+    }
+
+
+];
+
+
+//Function to create a template for the announcements section.
+function announcementsTemplate(items) {
+    return(
+        `
+        
+        <div class="list" >
+            <div class="teacherInfo">
+                <span class="name">PA: ${items.teacherName}</span>
+            </div>
+            <p class="items">${items.information}</p>
+            <span class="course">${items.course ?"Course: Mathematics 101":""}</span>
+            <div class="details">
+                <span class="details-content">${items.files}</span>
+                <span class="details-content">${items.timing}</span>
+            </div> 
+            
+        </div>
+        
+        `
+    )
+};
+
+//Getting the div to which we want to append the dynamically created elements.
+const announce = document.querySelector(".announce-items");
+ 
+//Appending the elements to the div.
+announce.innerHTML = `
+    ${announceItems.map(announcementsTemplate).join("")}
+`;
+
+//Mapping through the data and adding elements dynamically.
